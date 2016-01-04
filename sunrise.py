75a48704cfc283ea0044ef9669a6e118f9789e2c
Michael Garvin <mfjgarvin@gmail.com>
2016-01-04 17:52:08 -0500

    Successful integration of scheduling by time

--- sunrise.py
+++ sunrise.py
@@ -1,15 +1,10 @@
 #Todo:
-#Make Incrimenting stop when value is reached
-#Make sure this works in revese
-#This isn't much, but this helps to define color codes for different colors!
-import random
 import subprocess
 import threading
 import time as timelib
 import web
 from web import form
 #Setting up the Pi...
-complete = 0
 bLed = 17
 gLed = 22
 rLed = 21
@@ -20,25 +15,24 @@ redvalue = 0
 greenvalue = 0
 bluevalue = 0
 reset = 0
+nosched = True
 #Web
 render = web.template.render('templates/')
 urls = ('/', 'index')
 app = web.application(urls, globals())
 
 myform = form.Form(
-    form.Radio(name='AM/PM', args=['AM', 'PM'],value='AM'),
     form.Radio(name='Style', args=['Increase', 'Decrease'],value='Increase'),
     form.Radio(name='Brightness', args=['Low', 'Medium', 'High'],value='Medium'),
     form.Dropdown(name='Hour', args=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],value='6'),
     form.Dropdown(name='Minute', args=['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'], value='30'),
+    form.Radio(name='AM/PM', args=['AM', 'PM'],value='AM'),
+    form.Checkbox('Now', id='Now', value='True'),
     form.Textbox("Length",
         form.notnull,
         form.regexp('\d+', 'Need a numerical length here...')))
-#    form.Textbox("Color",
-#       form.notnull))
 
-print("Welcome to the prototype sunrise alarm clock. Follow the prompts to decide on a color, fade in time, blink rate, etc")
-#Todo: Multiple colors here. Maybe a menu type prompt? (1 leads to A, 2 leads to B, etc)
+print("Welcome to the prototype sunrise alarm clock. Follow the online prompts to decide on a color, fade in time, blink rate, etc")
 def led(red,green,blue):
 	subprocess.call("echo '%d'='%f' > /dev/pi-blaster" % (rLed,red), shell=True)
 	subprocess.call("echo '%d'='%f' > /dev/pi-blaster" % (gLed,green), shell=True)
@@ -46,7 +40,6 @@ def led(red,green,blue):
 
 def fade(ired,igreen,iblue,time,direction):
     try:
-#	print('Fade Started')
 	global red
 	global green
 	global blue
@@ -57,7 +50,6 @@ def fade(ired,igreen,iblue,time,direction):
         redorig = red
         blueorig = blue
         greenorig = green
-#	print("Fade 2")
         print(red,green,blue)
         if direction == 1:
             red = 0
@@ -176,7 +168,6 @@ def fade(ired,igreen,iblue,time,direction):
 		    break		
     	led_event = threading.Event()
         led_event.set()
-#	print("Fade 3")
         redthread = threading.Thread(target=redfade)
         greenthread = threading.Thread(target=greenfade)
         bluethread = threading.Thread(target=bluefade)
@@ -196,110 +187,49 @@ def fade(ired,igreen,iblue,time,direction):
 	greenthread.join()
 	bluethread.join()
 	watchthread.join()
-def prompt():
-    try:
-	global complete
-        global redvalue
-	global greenvalue
-	global bluevalue
-	global time
-	global direction
-	#if complete == 1:
-	 #   break
-        while True:
-	    try:
-	        redvalue = input("Red Value: 0 - 100:")
-	        if 0 <= redvalue <= 100:
-	            break
-	        else:
-	            print ("I can't compute that. Try again?")
-            except (ValueError, NameError):
-                print ("I can't compute that. Try again?")
-	    except SyntaxError:
-	        redvalue = random.randint(0,100)
-		break
-	while True:
-	    try:
-	        greenvalue = input("Green Value: 0 - 100:")
-		if 0 <= greenvalue <= 100:
-		    break
-		else:
-		    print("I can't compute that. Try again?")
-	    except (ValueError, NameError):
-		print("I can't compute that. Try again?")
-	    except SyntaxError:
-		greenvalue = random.randint(0,100)
-		break
-        while True:
-	    try:
-	        bluevalue = input("Blue Value: 1 - 100:")
-	        if 0 <= bluevalue <= 100:
-		    break
-		else:
-		    print("I can't compute that. Try again?")
-	    except (ValueError, NameError):
-		print("I can't compute that. Try again?")
-	    except SyntaxError:
-		bluevalue = random.randint(0,100)
-		break
-        while True:
-	    try:
-		time = input("Fade-in Time (In Minutes)")
-		time = float(time)
-		if 0 < time or time == 0:
-		    break
-		else:
-		    print("I need an actual time...")
-	    except (ValueError, NameError): 
-		print("I don't speak alphabet.")
-	    except (SyntaxError):
-	        print("Please enter a time")
-	while True:
-	    try:
-		direction = input("Direction (ON = 1, OFF = 0)")
-		if direction == 0 or direction == 1:
-		    break
-		else:
-		    print("Need a 0 or 1 here, buddy")
-	    except:
-		print("Need a 0 or 1 here, buddy")
-        print
-        print("Working...")
-        print
-        print("Great! Take a look at the LED and if you like what you see, jot down these color values:")
-        print(redvalue, greenvalue, bluevalue)
-    except KeyboardInterrupt:
-        print	
-        print("Goodbye!")
-
-def startfade(red,green,blue,time,direction):
+def startfade(red,green,blue,time,direction,hour,minute,s):
     global reset
+    global running
+    global nosched
+    running = 1
     led(0,0,0)
     timelib.sleep(0.1)
-    led(0,1,0)
+    led(0,0.1,0)
     timelib.sleep(0.1)
     led(0,0,0)
     timelib.sleep(0.1)
-    led(0,1,0)
+    led(0,0.1,0)
     timelib.sleep(0.1)
     led(0,0,0)
     reset = 1
     timelib.sleep(0.5)
     reset = 0
-    fade(red,green,blue,time,direction)
-
+    if str(s) == 'PM':
+	if hour == 12:
+	    pass
+	else:
+	    hour = hour + 12
+    if str(s) == 'AM' and hour == 12:
+	hour = 0
+    while running == 1:
+        now = timelib.localtime()
+	if hour == now.tm_hour and minute == now.tm_min or nosched == 'True':
+	    fade(red,green,blue,time,direction)
+	    return
+        else:
+	    timelib.sleep(1)
 
 class index:
+    global nosched
     def GET(self):
         form = myform()
-        # make sure you create a copy of the form by calling it (line above)
-        # Otherwise changes will appear globally
         return render.formtest(form)
-	print ("Page Opened")
     def POST(self):
 	global redvalue
 	global greenvalue
 	global bluevalue
+	global running
+	global nosched
 	def hex_to_rgb(value):
 	    global redvalue
 	    global greenvalue
@@ -309,22 +239,24 @@ class index:
 	    redvalue = (100*float(color[0]))/255
 	    greenvalue = (100*float(color[1]))/255
 	    bluevalue = (100*float(color[2]))/255
-	    print("Colors Calculated")
         form = myform()
         if not form.validates():
             return render.formtest(form)
         else:
-            # form.d.boe and form['boe'].value are equivalent ways of
-            # extracting the validated arguments from the form.
 	    color = web.input().newcolor
 	    time = form.d.Length
+	    hour = form.d.Hour
+	    minute = form.d.Minute
+	    try:
+		nosched = str(web.input().Now)
+	    except AttributeError:
+		nosched = False
+	    s = form['AM/PM'].value
 	    if form.d.Style == 'Increase':
 		style = 1
 	    else:
 		style = 0	    
-#	    style = form.d.Style
 	    hex_to_rgb(color)
-#	    print(color,redvalue,greenvalue,bluevalue,int(time),style)
 	    if form.d.Brightness == 'Low':
 		redvalue = float(redvalue) * 0.33
 		greenvalue = float(greenvalue) * 0.33
@@ -334,14 +266,15 @@ class index:
 		greenvalue = float(greenvalue) * 0.66
 		bluevalue = float(bluevalue) * 0.66
 	    elif form.d.Brightness == 'High':
-		#Do Nothin
+		#Do Nothing
 		pass
-	    ledthread=threading.Thread(target=startfade, args=(float(redvalue),float(greenvalue),float(bluevalue),float(time),style))
-            ledthread.start()
-#	    fade(float(redvalue),float(greenvalue),float(bluevalue),float(time),style)	
-            return "Done. Color: %s, %f, %f, %f. Time: %d Style: %d" % (str(color), float(redvalue), float(greenvalue), float(bluevalue), int(time), int(style))
+	    ledthread=threading.Thread(target=startfade, args=(float(redvalue),float(greenvalue),float(bluevalue),float(time),style,int(hour),int(minute),str(s)))
+	    while ledthread.isAlive():
+		running = 0
+		time.sleep(0.5)
+	    if not ledthread.isAlive():
+		ledthread.start()
+            return "Done. Color: %s, %f, %f, %f. Time: %d Style: %d Schedule: %d:%d %s Now? %s " % (str(color), float(redvalue), float(greenvalue), float(bluevalue), int(time), int(style), int(hour), int(minute), str(s), nosched)
 if __name__=="__main__":
     web.internalerror = web.debugerror
     app.run()
-#prompt()
-#fade(redvalue,greenvalue,bluevalue,time,direction)
